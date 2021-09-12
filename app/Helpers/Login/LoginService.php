c629e6774b3b2afde980a440e0a26b7b609765f5
alikaregari <sina98.saberi@gmail.com>
2021-09-12 22:41:37 +0430

    Update LoginService.php

--- app/Helpers/Login/LoginService.php
+++ app/Helpers/Login/LoginService.php
@@ -1,14 +1,25 @@
 <?php
 
 namespace app\Helpers\Login;
+use Illuminate\Support\Str;
+
 class LoginService
 {
-    public function UserLogin(): bool
-    {
-        if (auth()->check()):
-            return true;
-        else:
-            return false;
-        endif;
-    }
+   protected $cart;
+   public function __construct()
+   {
+       $this->cart=session()->get('cart') ?? collect([]);
+   }
+   public function put($value,$obj){
+       $value=array_merge($value,[
+           'id'=>Str::random(10),
+           'subject_id'=>$obj->id,
+           'subject_type'=>get_class($obj)
+       ]);
+       $this->cart->put($value['id'],$value);
+       session()->put('cart',$this->cart);
+   }
+   public function has($obj){
+       return $this->cart->where('subject_id',$obj->id)->where('subject_type',get_class($obj))->first();
+   }
 }
