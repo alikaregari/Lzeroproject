8742002527950e7573639e97607dbed0c6374ec5
alikaregari <sina98.saberi@gmail.com>
2021-09-13 19:49:33 +0430

    Update Cart.php

--- app/Helpers/Cart/Cart.php
+++ app/Helpers/Cart/Cart.php
@@ -2,8 +2,17 @@
 
 namespace app\Helpers\Cart;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class Cart
+ * @package app\Helpers\Cart
+ * @method static bool has(string $id)
+ * @method static Collection all()
+ * @method static Cart put(array $value,Model $obj)
+ */
 class Cart extends Facade
 {
     protected static function getFacadeAccessor(): string
