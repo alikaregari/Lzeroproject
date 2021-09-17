9fe1539271b834c8f5074c1684bc7c3c1a13a609
alikaregari <sina98.saberi@gmail.com>
2021-09-17 13:26:40 +0430

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -14,7 +14,28 @@ class CartController extends Controller
         $this->call=collect([]);
     }
     public function index(){
-        //return Cart::all();
+    /*    $this->call->put(1,[
+            'quantity'=>2,
+            'price'=>25000,
+            'product'=>[
+                'name'=>'مرحله اول',
+                'id'=>12
+            ]
+        ]);
+        $this->call->put(2,[
+            'quantity'=>3,
+            'price'=>35000,
+            'product'=>[
+                'name'=>'مرحله اول',
+                'id'=>13
+            ]
+        ]);
+        $item=collect($this->call->get(1));
+        $item=$item->merge([
+            'quantity'=>7
+        ]);
+        $this->call->put(1,$item);
+        return $this->call;*/
         $cart=Cart::all();
         return view('products.cart',compact('cart'));
     }
@@ -38,7 +59,6 @@ class CartController extends Controller
             $data['quantity']=$data['quantity']+1;
         endif;
         unset($data['btn']);
-        return $data;
         if (Cart::has($data['id'])):
             Cart::update($data['id'],[
                 'quantity'=>$data['quantity']
